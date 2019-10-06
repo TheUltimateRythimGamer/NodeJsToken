c4d8819dff6e62b7b65dd3c21b6733bca37930f9
JossJoestar <oreoslawiet@gmail.com>
2019-10-06 12:56:28 -0700

    Create initial Route

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const auth_1 = __importDefault(require("./routes/auth"));
+const app = express_1.default();
+/**
+ * Settings
+ */
+app.set('port', 4000);
+/**
+ * Routers
+ */
+app.use(auth_1.default);
+exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
 import express, {Application} from "express";
+import authRoutes from "./routes/auth";
 
 const app : Application = express();
 
+/**
+ * Settings
+ */
+app.set('port', 4000);
+
+/**
+ * Routers 
+ */
+app.use(authRoutes);
+
 export default app;
+
