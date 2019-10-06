351e7bd63485a50d476b2a7675cba7a53a04397e
JossJoestar <oreoslawiet@gmail.com>
2019-10-06 14:08:46 -0700

    Create Model Schema

---
+++ dist/models/User.Model.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const userSchema = new mongoose_1.Schema({
+    userName: {
+        type: String,
+        required: true,
+        min: 4,
+        lowercase: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+exports.default = mongoose_1.model('User', userSchema);
+//# sourceMappingURL=User.Model.js.map
